21d0b885e5ff9e3cf7e8bf7f129332b30e4f9624
SaschaAthmer <33932092+J0hnExample@users.noreply.github.com>
2024-01-07 16:28:48 +0100

    added shared pointer and comments

--- Smart_Pointer/shared_ptr/main.cpp
+++ Smart_Pointer/shared_ptr/main.cpp
@@ -1,12 +1,20 @@
-//shared_ptr 
-/*  */
+// shared_ptr
+// This code demonstrates the use of shared_ptr for shared resource management, 
+// and unique_ptr for exclusive resource ownership. shared_ptr allows multiple instances 
+// to own the same resource, using reference counting to ensure proper resource release. 
+// unique_ptr is used for single ownership, automatically managing the resource's lifecycle. 
+// Ownership can be transferred from unique_ptr to shared_ptr, ensuring memory safety and 
+// efficient resource management in a shared context.
+// unique_ptr are faster and more memory efficient than shared_ptr.
+
 
 #include <iostream>
 #include <memory>
 
-#define myptr std::unique_ptr
+// Defining a shorthand for std::shared_ptr for convenience
+#define myptr std::shared_ptr
 
-// Using the standard namespace to avoid prefixing std:: to standard library functions
+// Use the standard namespace to avoid prefixing std:: to standard library functions
 using namespace std;
 
 // Define a struct 'MyVector' to handle a dynamic array of integers
@@ -15,14 +23,13 @@ struct MyVector {
     unique_ptr<int[]> mData = nullptr; // Unique pointer to an array of integers
 };
 
+// Function to read data into a MyVector and return a shared_ptr to it
 myptr<MyVector> readData(){
-    // Create a unique_ptr to MyVector, dynamically allocating memory for it
+    // Dynamically allocate memory for MyVector using unique_ptr
     auto myVec = unique_ptr<MyVector>(new MyVector);
     int num = 0;
 
-    //auto myVecCopy = myVec; // This line will cause a compile-time error
-
-    // Prompt the user to enter the number of elements
+    // Prompt user to enter the number of elements
     cout << "Element Count?" << endl;
     cin >> num;
 
@@ -35,17 +42,14 @@ myptr<MyVector> readData(){
         cout << "Element " << i << " Data?: ";
         cin >> myVec->mData[i];
     }
-    
+
+    // Return myVec as a shared_ptr for shared ownership
     return myVec;
-
-    
-
-    // The unique_ptr automatically deletes the MyVector object when it goes out of scope
-
 };
-void printData(myptr<MyVector> myVec){
 
-    // Display all elements in the array
+// Function to print data from a MyVector pointed to by a shared_ptr
+void printData(myptr<MyVector> myVec){
+    // Display all elements in the array and calculate their sum
     cout << "Element Data: " << endl;
     int sum = 0;
     for (size_t i = 0; i < myVec->mNum; i++) {
@@ -54,9 +58,11 @@ void printData(myptr<MyVector> myVec){
     }
     cout << "Element Sum: " << sum << endl;  
 }
+
 int main() {
+    // Read data into myVec and then print it
     auto myVec = readData();
     printData(myVec);
+
     return 0;
 };
-
